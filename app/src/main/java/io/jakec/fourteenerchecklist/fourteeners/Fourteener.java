95375152f5ea9b6b0a7babf4ad9bd395395deaa5
Jake Crawford <jake.crawford@outlook.com>
2018-01-27 15:34:32 -0700

    Add XML based data access implementation.

--- app/src/main/java/io/jakec/fourteenerchecklist/fourteeners/Fourteener.java
+++ app/src/main/java/io/jakec/fourteenerchecklist/fourteeners/Fourteener.java
@@ -7,6 +7,42 @@ public class Fourteener {
     private String range;
     private int elevation;
 
+    public Fourteener(int id, String name, String range, int elevation) {
+        this.id = id;
+        this.name = name;
+        this.range = range;
+        this.elevation = elevation;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public int getElevation() {
+        return elevation;
+    }
+
+    public void setElevation(int elevation) {
+        this.elevation = elevation;
+    }
 }
---
+++ app/src/main/java/io/jakec/fourteenerchecklist/fourteeners/XmlFourteenerDaoImpl.java
@@ -0,0 +1,102 @@
+package io.jakec.fourteenerchecklist.fourteeners;
+
+import android.support.annotation.Nullable;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+public class XmlFourteenerDaoImpl implements FourteenerDao {
+    private final String xmlSource = "peaks.xml";
+    private final String xmlNodeName = "peak";
+
+    @Override
+    public List<Fourteener> getAllFourteeners() {
+        try {
+            NodeList peakNodeList = getPeakNodeList();
+            return createFourteenerListFromNodeList(peakNodeList);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    public List<Fourteener> getFourteenersByRange(String range) {
+        List<Fourteener> fourteenersInRange = new ArrayList<>();
+
+        List<Fourteener> fourteenerList = getAllFourteeners();
+        for (Fourteener currentFourteener: fourteenerList) {
+            if (currentFourteener.getRange() == range) {
+                fourteenersInRange.add(currentFourteener);
+            }
+        }
+
+        return fourteenersInRange;
+    }
+
+    @Override
+    public Fourteener getFourteenerById(int id) {
+        List<Fourteener> fourteenerList = getAllFourteeners();
+        for (Fourteener currentFourteener: fourteenerList) {
+            if (currentFourteener.getId() == id) {
+                return currentFourteener;
+            }
+        }
+
+        return null;
+    }
+
+    private NodeList getPeakNodeList() throws Exception {
+        File xmlFile = new File(this.xmlSource);
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(xmlFile);
+        document.getDocumentElement().normalize();
+
+        return document.getElementsByTagName(xmlNodeName);
+    }
+
+    private List<Fourteener> createFourteenerListFromNodeList(NodeList nodeList) {
+        List<Fourteener> fourteenerList = new ArrayList<>();
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node currentNode = nodeList.item(i);
+            if (currentNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element currentElement = (Element) currentNode;
+                Fourteener currentFourteener = getFourteenerFromElementNode(currentElement);
+
+                if (currentFourteener != null) {
+                    fourteenerList.add(currentFourteener);
+                }
+            }
+        }
+
+        return fourteenerList;
+    }
+
+    @Nullable
+    private Fourteener getFourteenerFromElementNode(Element currentElement) {
+        try {
+            int id = Integer.parseInt(currentElement.getAttribute("rank"));
+            String name = currentElement.getAttribute("name");
+            String range = currentElement.getAttribute("range");
+            int elevation = Integer.parseInt(currentElement.getAttribute("elevation"));
+
+            return new Fourteener(id, name, range, elevation);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
